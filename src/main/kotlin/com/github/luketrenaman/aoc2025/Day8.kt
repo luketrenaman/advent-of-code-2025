e187e7f42cd703873da4e1be78f3402b6481b340
Luke Trenaman <trenamanluke@gmail.com>
2025-12-08 12:50:40 -0500

    shorten

--- src/main/kotlin/com/github/luketrenaman/aoc2025/Day8.kt
+++ src/main/kotlin/com/github/luketrenaman/aoc2025/Day8.kt
@@ -5,34 +5,24 @@ import kotlin.math.sqrt
 
 class Day8 {
     data class WeightedEdge(val edge: Pair<Int, Int>, val weight: Double)
+    fun sqd(x: Double,y: Double) = (x-y) * (x-y)
+    fun dist(coord1: List<Int>, coord2: List<Int>): Double =
+        sqrt(coord1.zip(coord2).sumOf { sqd(it.first.toDouble(), it.second.toDouble()) })
     fun part1(input: File): Long {
-        fun sqd(x: Double,y: Double) = (x-y) * (x-y)
-        fun dist(coord1: List<Int>, coord2: List<Int>): Double =
-            sqrt(coord1.zip(coord2).sumOf { sqd(it.first.toDouble(), it.second.toDouble()) })
-
         val coords = input.readLines().map{ line -> line.split(",").map{it.toInt()}}
         val edges = mutableListOf<WeightedEdge>()
         for(i in coords.indices){
             for(j in (i+1)until(coords.size)){
-                if(coords[i] != coords[j]){
-                    edges.add(WeightedEdge(i to j, dist(coords[i], coords[j])))
-                }
+                edges.add(WeightedEdge(i to j, dist(coords[i], coords[j])))
             }
         }
-        edges.sortBy{
-            it.weight
-        }
+        edges.sortBy{ it.weight }
         var edgesToAdd = if(coords.size == 1000) 1000 else 10
-        val trees = mutableListOf<MutableSet<Int>>()
-        for(i in coords.indices){
-            trees.add(mutableSetOf(i))
-        }
+        val trees = coords.indices.map{mutableSetOf(it)}.toMutableList()
         while(edgesToAdd > 0){
-            val weightedEdge = edges.removeAt(0)
-            val edge = weightedEdge.edge
-            val s1 = trees.find{it.contains(edge.first)}
-            val s2 = trees.find{it.contains(edge.second)}
-            //println("Connecting ${coords[edge.first]}, ${coords[edge.second]}")
+            val edge = edges.removeAt(0).edge
+            val s1 = trees.find{edge.first in it}
+            val s2 = trees.find{edge.second in it}
             if(s1 != s2){
                 s1!!.addAll(s2!!)
                 trees.remove(s2)
@@ -43,10 +33,6 @@ class Day8 {
         return trees.map{it.size.toLong()}.sortedDescending().take(3).reduce(Long::times)
     }
     fun part2(input: File): Long {
-        fun sqd(x: Double,y: Double) = (x-y) * (x-y)
-        fun dist(coord1: List<Int>, coord2: List<Int>): Double =
-            sqrt(coord1.zip(coord2).sumOf { sqd(it.first.toDouble(), it.second.toDouble()) })
-
         val coords = input.readLines().map{ line -> line.split(",").map{it.toInt()}}
         val edges = mutableListOf<WeightedEdge>()
         for(i in coords.indices){
@@ -56,20 +42,13 @@ class Day8 {
                 }
             }
         }
-        edges.sortBy{
-            it.weight
-        }
-        val trees = mutableListOf<MutableSet<Int>>()
-        for(i in coords.indices){
-            trees.add(mutableSetOf(i))
-        }
+        edges.sortBy{ it.weight }
+        val trees = coords.indices.map{mutableSetOf(it)}.toMutableList()
         var edge: Pair<Int, Int> = edges[0].edge
         while(trees.size > 1){
-            val weightedEdge = edges.removeAt(0)
-            edge = weightedEdge.edge
-            val s1 = trees.find{it.contains(edge.first)}
-            val s2 = trees.find{it.contains(edge.second)}
-            //println("Connecting ${coords[edge.first]}, ${coords[edge.second]}")
+            edge = edges.removeAt(0).edge
+            val s1 = trees.find{edge.first in it}
+            val s2 = trees.find{edge.second in it}
             if(s1 != s2){
                 s1!!.addAll(s2!!)
                 trees.remove(s2)
