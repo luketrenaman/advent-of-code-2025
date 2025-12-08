f22acafb5384e6a9deb286803e6c62b1db9470e5
Luke Trenaman <trenamanluke@gmail.com>
2025-12-08 12:08:07 -0500

    solve day 8

--- src/main/kotlin/com/github/luketrenaman/aoc2025/Day8.kt
+++ src/main/kotlin/com/github/luketrenaman/aoc2025/Day8.kt
@@ -1,18 +1,19 @@
 package com.github.luketrenaman.aoc2025
 
 import java.io.File
+import kotlin.math.sqrt
 
 class Day8 {
     data class WeightedEdge(val edge: Pair<Int, Int>, val weight: Double)
     fun part1(input: File): Long {
         fun sqd(x: Double,y: Double) = (x-y) * (x-y)
         fun dist(coord1: List<Int>, coord2: List<Int>): Double =
-            coord1.zip(coord2).sumOf { sqd(it.first.toDouble(), it.second.toDouble()) }
+            sqrt(coord1.zip(coord2).sumOf { sqd(it.first.toDouble(), it.second.toDouble()) })
 
         val coords = input.readLines().map{ line -> line.split(",").map{it.toInt()}}
         val edges = mutableListOf<WeightedEdge>()
         for(i in coords.indices){
-            for(j in (coords.indices - i)){
+            for(j in (i+1)until(coords.size)){
                 if(coords[i] != coords[j]){
                     edges.add(WeightedEdge(i to j, dist(coords[i], coords[j])))
                 }
@@ -21,19 +22,60 @@ class Day8 {
         edges.sortBy{
             it.weight
         }
-        var edgesToAdd = 10
-        val adj = mutableMapOf<Int, MutableList<Int>>()
+        var edgesToAdd = if(coords.size == 1000) 1000 else 10
+        val trees = mutableListOf<MutableSet<Int>>()
+        for(i in coords.indices){
+            trees.add(mutableSetOf(i))
+        }
         while(edgesToAdd > 0){
-            val edge = edges.removeAt(0).edge
-            if(!adj.contains(edge.first)) adj[edge.first] = mutableListOf()
-            if(!adj.contains(edge.second)) adj[edge.second] = mutableListOf()
-            adj[edge.first]!!.add(edge.second)
-            adj[edge.second]!!.add(edge.first)
+            val weightedEdge = edges.removeAt(0)
+            val edge = weightedEdge.edge
+            val s1 = trees.find{it.contains(edge.first)}
+            val s2 = trees.find{it.contains(edge.second)}
+            //println("Connecting ${coords[edge.first]}, ${coords[edge.second]}")
+            if(s1 != s2){
+                s1!!.addAll(s2!!)
+                trees.remove(s2)
+            }
             edgesToAdd--
         }
+        //println(trees)
+        return trees.map{it.size.toLong()}.sortedDescending().take(3).reduce(Long::times)
     }
     fun part2(input: File): Long {
-        return -1
+        fun sqd(x: Double,y: Double) = (x-y) * (x-y)
+        fun dist(coord1: List<Int>, coord2: List<Int>): Double =
+            sqrt(coord1.zip(coord2).sumOf { sqd(it.first.toDouble(), it.second.toDouble()) })
+
+        val coords = input.readLines().map{ line -> line.split(",").map{it.toInt()}}
+        val edges = mutableListOf<WeightedEdge>()
+        for(i in coords.indices){
+            for(j in (i+1)until(coords.size)){
+                if(coords[i] != coords[j]){
+                    edges.add(WeightedEdge(i to j, dist(coords[i], coords[j])))
+                }
+            }
+        }
+        edges.sortBy{
+            it.weight
+        }
+        val trees = mutableListOf<MutableSet<Int>>()
+        for(i in coords.indices){
+            trees.add(mutableSetOf(i))
+        }
+        var edge: Pair<Int, Int> = edges[0].edge
+        while(trees.size > 1){
+            val weightedEdge = edges.removeAt(0)
+            edge = weightedEdge.edge
+            val s1 = trees.find{it.contains(edge.first)}
+            val s2 = trees.find{it.contains(edge.second)}
+            //println("Connecting ${coords[edge.first]}, ${coords[edge.second]}")
+            if(s1 != s2){
+                s1!!.addAll(s2!!)
+                trees.remove(s2)
+            }
+        }
+        return coords[edge.first][0].toLong() * coords[edge.second][0].toLong()
     }
 }
 
--- src/test/kotlin/com/github/luketrenaman/aoc2025/Day8Test.kt
+++ src/test/kotlin/com/github/luketrenaman/aoc2025/Day8Test.kt
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.Test
 
 class Day8Test {
     @Test fun `Part 1 Example`(){
-        assertPartEquals(0L, "day8", "example", Day8()::part1)
+        assertPartEquals(40L, "day8", "example", Day8()::part1)
     }
 
     @Test fun `Part 1 Solution`(){
-        assertPartEquals(0L, "day8", "input", Day8()::part1)
+        assertPartEquals(32103L, "day8", "input", Day8()::part1)
     }
 
     @Test fun `Part 2 Example`(){
-        assertPartEquals(0L, "day8", "example", Day8()::part2)
+        assertPartEquals(25272L, "day8", "example", Day8()::part2)
     }
 
     @Test fun `Part 2 Solution`(){
-        assertPartEquals(0L, "day8", "input", Day8()::part2)
+        assertPartEquals(8133642976L, "day8", "input", Day8()::part2)
     }
 }
 
